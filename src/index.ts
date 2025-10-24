32e6bb5dff4d1442aa0a0502cb9976b85261e6c8
chijiokerichard <chijiokerichard61@gmail.com>
2025-10-24 04:27:13 -0700

    my frist commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// mport express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express API!");
+});
+app.listen(process.env.PORT, () => console.log(`Server running on port http://localhost:3000`));
+
+export default app; // ✅ Required for Vercel
